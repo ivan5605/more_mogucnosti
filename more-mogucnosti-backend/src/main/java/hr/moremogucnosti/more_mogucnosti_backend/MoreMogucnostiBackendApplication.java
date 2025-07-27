b8b3c8283f379f0609388631486af308c3f87b45
ivan5605 <ivan.smesnik1@gmail.com>
2025-07-27 16:50:30 +0200

    prvi commit

---
+++ more-mogucnosti-backend/src/main/java/hr/moremogucnosti/more_mogucnosti_backend/MoreMogucnostiBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.moremogucnosti.more_mogucnosti_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreMogucnostiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreMogucnostiBackendApplication.class, args);
+	}
+
+}
---
+++ more-mogucnosti-backend/src/test/java/hr/moremogucnosti/more_mogucnosti_backend/MoreMogucnostiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.moremogucnosti.more_mogucnosti_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreMogucnostiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
